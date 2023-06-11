c559650c21684bca8ba8a7c73c1d1c68b46fd633
sheehan-j <jordansheehan@ufl.edu>
2023-06-11 11:32:59 -0400

    Add icons to navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -4,6 +4,12 @@ import colors from "../config/colors";
 const Navbar = ({ setScanModalVisible, setProduct }) => {
 	return (
 		<View style={styles.container}>
+			<Pressable style={styles.iconContainer}>
+				<Image
+					source={require("../assets/home_icon_dark.png")}
+					style={styles.icon}
+				/>
+			</Pressable>
 			<Pressable
 				style={({ pressed }) => [
 					{
@@ -14,13 +20,19 @@ const Navbar = ({ setScanModalVisible, setProduct }) => {
 					styles.scanIcon,
 				]}
 				onPressOut={() => {
+					setProduct(null); // Clear out any existing product data
 					setScanModalVisible(true);
-					setProduct(null); // Clear out any existing barcode
 				}}
 			>
 				<Image
 					source={require("../assets/scan_icon.png")}
 					style={styles.scanIconImg}
+				/>
+			</Pressable>
+			<Pressable style={styles.iconContainer}>
+				<Image
+					source={require("../assets/home_icon.png")}
+					style={styles.icon}
 				/>
 			</Pressable>
 		</View>
@@ -39,19 +51,35 @@ const styles = StyleSheet.create({
 			width: 0,
 			height: -2,
 		},
-		shadowOpacity: 0.06,
-		shadowRadius: 1.5,
+		shadowOpacity: 0.02,
+		shadowRadius: 2.5,
 		flexDirection: "row",
-		justifyContent: "center",
+		justifyContent: "space-evenly",
 		alignContent: "center",
 		paddingTop: 13,
+		// paddingLeft: 20,
+		// paddingRight: 20,
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+	},
+	iconContainer: {
+		position: "relative",
+		height: 58,
+		width: 58,
+		padding: 14,
+	},
+	icon: {
+		height: "100%",
+		width: "100%",
 	},
 	scanIcon: {
 		position: "relative",
-		height: 60,
-		width: 60,
-		borderRadius: "50%",
-		padding: 16,
+		height: 55,
+		width: 55,
+		borderRadius: 30,
+		padding: 14,
+		// marginLeft: 50,
+		// marginRight: 50,
 	},
 	scanIconImg: {
 		height: "100%",
