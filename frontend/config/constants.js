a6196832a7e3dcac30b3f135b66a1c35afb176d6
sheehan-j <jordansheehan@ufl.edu>
2023-07-03 19:00:20 -0400

    Add max recent scans and saved products as constants

--- frontend/config/constants.js
+++ frontend/config/constants.js
@@ -3,6 +3,6 @@ import { ENV } from "@env";
 export const config = {
 	FOOD_API_BASE: "https://world.openfoodfacts.org/api/v2/product/",
 	API_URL: ENV === "dev" ? "http://10.1.154.176:6202/" : "",
-	MAX_RECENT_SCANS: 20,
-	MAX_SAVED_PRODUCTS: 75,
+	MAX_RECENT_SCANS: 2,
+	MAX_SAVED_PRODUCTS: 1,
 };
