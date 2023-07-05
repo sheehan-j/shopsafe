b602c3647100687696c59024ba8952e517e9b635
sheehan-j <jordansheehan@ufl.edu>
2023-07-05 18:15:16 -0400

    Folder structure change

--- backend/routes/users.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import * as usersController from "../controllers/usersController.js";
-const router = express.Router();
-
-router.route("/email").get(usersController.getUserByEmail);
-
-export default router;
