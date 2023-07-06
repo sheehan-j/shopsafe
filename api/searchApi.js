e5ba4daf757309147c5604834990289dd3ec9a99
sheehan-j <jordansheehan@ufl.edu>
2023-07-05 23:13:34 -0400

    Reformat product names from API

--- api/searchApi.js
+++ api/searchApi.js
@@ -28,10 +28,29 @@ exports.search = async (barcode, allergies) => {
 			result.product.ingredients
 		);
 
+		// Since some product names are poorly capitalized from the API, correct that there
+		// Split the name string by its spaces, capitalzie the first letter of each
+		// subsequent string, and recombine them into one properly capitalized string
+		const splitName = result.product.product_name.split(" ");
+		let formattedName = "";
+		for (let i = 0; i < splitName.length; i++) {
+			if (splitName[i].length > 1) {
+				formattedName +=
+					splitName[i]?.charAt(0)?.toUpperCase() +
+					splitName[i]?.slice(1);
+			} else {
+				formattedName += splitName[i];
+			}
+
+			if (i < splitName.length - 1) {
+				formattedName += " ";
+			}
+		}
+
 		return {
 			status: 1,
 			code: result.code,
-			name: result.product.product_name,
+			name: formattedName,
 			keywords: result.product._keywords,
 			brands: result.product.brands,
 			categories: result.product.categories,
